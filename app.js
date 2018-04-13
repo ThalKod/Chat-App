fbf3621ea5e2b20e211bcd7660ec1d13d0521b0f
ThalKod <tmarcelin@thal.tech>
2018-04-13 11:28:11 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const port    = process.env.PORT || 3000;
+
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+app.listen(port, ()=>{
+    console.log("Listening on port" + port);
+});
+
