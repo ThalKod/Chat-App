1305c643d9eeb05e623c2c3ecb53f9b78f69e572
ThalKod <tmarcelin@thal.tech>
2018-04-13 13:53:58 -0400

    update path

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var socket = io();
+
+socket.on("connect", function(){
+    console.log("Connected to Server");
+});
+
+socket.on("disconnect", function(){
+    console.log("Disconnected to server");
+});
+
+socket.on("newMessage", function(message){
+    console.log("New Message !", message);
+});
