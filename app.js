f49d1cc240640f3b3f3838d9883aba4691022db3
ThalKod <tmarcelin@thal.tech>
2018-04-16 09:18:20 -0400

    create message utility

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http      = require("http");
 const port      = process.env.PORT || 3000;
 const socketIO  = require("socket.io");
 
+const {generateMessage} = require("./utils/message");
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -12,26 +13,14 @@ app.use(express.static(__dirname + "/public"));
 io.on("connection", (socket) =>{
     console.log("New User connected");
 
-    socket.emit("newMessage", {
-        from: 'Admin',
-        text: "Welcome to the Chat Room",
-        createdAt: Date.now()
-    });
+    socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app"));
 
-    socket.broadcast.emit("newMessage", {
-        from: "Admin",
-        text: "New User Join",
-        createdAt: Date.now()
-    });
+    socket.broadcast.emit("newMessage", generateMessage("Admin", "New User Join"));
 
     socket.on("createdMessage", (message)=>{
         console.log("Message Created",message);
 
-        io.emit("newMessage",{
-            from: message.from,
-            text: message.text,
-            createdAt: Date.now()
-        });
+        io.emit("newMessage",generateMessage(message.from , message.text));
 
         // socket.broadcast.emit("newMessage", {
         //     rom: message.from,
